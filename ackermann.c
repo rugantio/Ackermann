8ca8d513b1450018cd33fadd4196dd39f517b63d
rugantio <costa@caos.space>
2018-02-19 15:54:05 +0100

    minor change

--- ackermann.c
+++ ackermann.c
@@ -20,6 +20,6 @@ int main()
  
 	clock_t end = clock();
 	double time_spent = (double) (end - begin) / CLOCKS_PER_SEC;
-	printf("Time spent computing was: %f", time_spent);
+	printf("Time spent computing was: %f\n", time_spent);
         return 0;
 }
