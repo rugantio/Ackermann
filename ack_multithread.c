73ff2ad1a2af02ca4db4f3cefd1af0a78a093767
rugantio <costa@caos.space>
2018-02-21 15:52:11 +0100

    better than before

--- ack_multithread.c
+++ ack_multithread.c
@@ -2,21 +2,33 @@
 #include <pthread.h>
 #include <time.h>
 
+#define THREADS 4
+
 int ackermann(int m, int n)
 {
         if (!m) return n + 1;
         if (!n) return ackermann(m - 1, 1);
         return ackermann(m - 1, ackermann(m, n - 1));
 }
- 
+
+void* f(void* data) {
+  printf("Hello from a thread!\n");
+  pthread_exit(NULL);
+}
+
 int main()
 {
         int m, n;
-        
+	pthread_t threads[THREADS];        
+	
 	clock_t tic = clock();
 	
-	pthread_create((&t,NULL,ackermann,(void*)&num));	
-	
+	int t;
+  	for (t = 0; t < THREADS; t++) {
+   		pthread_create(&threads[t], NULL, f, NULL);
+	}
+
+
 	for (m = 0; m <= 5; m++)
 		for (n = 0; n < 6 - m; n++)
 			printf("A(%d, %d) = %d\n", m, n, ackermann(m, n));
@@ -25,5 +37,5 @@ int main()
 	
 	double time_spent = (double) (toc - tic) / CLOCKS_PER_SEC;
 	printf("Time spent computing was: %f seconds\n", time_spent);
-        return 0;
+        pthread_exit(NULL);
 }
